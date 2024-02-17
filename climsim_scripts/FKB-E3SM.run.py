7858d9eff0b319c7ee66a65ccb12556215b6f323
zyhu-hu <zeyuan_hu@fas.harvard.edu>
2024-02-16 21:55:55 -0800

    change test casename

--- climsim_scripts/FKB-E3SM.run.py
+++ climsim_scripts/FKB-E3SM.run.py
@@ -12,7 +12,7 @@ newcase,config,build,clean,submit,continue_run = False,False,False,False,False,F
 
 acct = 'm4331'
 
-case_prefix = 'torch_cpu2'
+case_prefix = 'torch_cpu3'
 # Added extra physics_state and cam_out variables.
 
 top_dir  = os.getenv('HOME')
