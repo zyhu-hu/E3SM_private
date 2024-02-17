e5467013f66c156dd5b1d9e9bae42d45827c2106
zyhu-hu <zeyuan_hu@fas.harvard.edu>
2024-02-17 00:00:03 -0800

    try build with gnucpu

--- cime_config/machines/cmake_macros/gnu_pm-cpu.cmake
+++ cime_config/machines/cmake_macros/gnu_pm-cpu.cmake
@@ -10,3 +10,7 @@ set(MPIFC "ftn")
 set(SCC "gcc")
 set(SCXX "g++")
 set(SFC "gfortran")
+
+# FKB (Sungduk Yu, Thu 21 Sep 2023 05:12:11 PM PDT)
+string(APPEND CMAKE_Fortran_FLAGS " -I/global/cfs/cdirs/m4331/shared/FKB64/build/include ")
+string(APPEND CMAKE_EXE_LINKER_FLAGS " -L/global/cfs/cdirs/m4331/shared/FKB64/build/lib -lneural ")
