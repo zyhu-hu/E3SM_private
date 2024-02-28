c832f25566ac39101a89f79e85a7b30ae2256250
zyhu-hu <zeyuan_hu@fas.harvard.edu>
2024-02-28 14:31:38 -0800

    update a submit script

--- climsim_scripts/FKB-E3SM.run.py
+++ climsim_scripts/FKB-E3SM.run.py
@@ -12,7 +12,7 @@ newcase,config,build,clean,submit,continue_run = False,False,False,False,False,F
 
 acct = 'm4331'
 
-case_prefix = 'v3_unet_gputest'
+case_prefix = 'v3_unet_batchinf'
 # Added extra physics_state and cam_out variables.
 
 top_dir  = os.getenv('HOME')
