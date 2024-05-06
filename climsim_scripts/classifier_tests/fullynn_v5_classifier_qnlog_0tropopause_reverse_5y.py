3e1707d9d7c5c5a757aa51e3937954411a6faf48
zyhu-hu <zeyuan_hu@fas.harvard.edu>
2024-05-06 00:23:14 -0700

    change 2 node to 1node for reverse_5y

--- climsim_scripts/classifier_tests/fullynn_v5_classifier_qnlog_0tropopause_reverse_5y.py
+++ climsim_scripts/classifier_tests/fullynn_v5_classifier_qnlog_0tropopause_reverse_5y.py
@@ -49,7 +49,7 @@ dtime = 1200 # set to 0 to use a default value
 #stop_opt,stop_n,resub,walltime = 'nmonths',1, 1, '00:30:00'
 stop_opt,stop_n,resub,walltime = 'nmonths',60, 0,'24:00:00'
 
-ne,npg=4,2;  num_nodes=2  ; grid=f'ne{ne}pg{npg}_ne{ne}pg{npg}'
+ne,npg=4,2;  num_nodes=1  ; grid=f'ne{ne}pg{npg}_ne{ne}pg{npg}'
 # ne,npg=30,2; num_nodes=32 ; grid=f'ne{ne}pg{npg}_ne{ne}pg{npg}'
 # ne,npg=30,2; num_nodes=32 ; grid=f'ne{ne}pg{npg}_oECv3' # bi-grid for AMIP or coupled
 
