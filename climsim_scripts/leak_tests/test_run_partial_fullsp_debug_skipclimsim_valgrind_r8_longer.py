f4e2c7d9809358e96969a4516396dbdda46562d1
zyhu-hu <zeyuan_hu@fas.harvard.edu>
2024-05-07 20:42:27 -0700

    add a longer run test

--- climsim_scripts/leak_tests/test_run_partial_fullsp_debug_skipclimsim_valgrind_r8_longer.py
+++ climsim_scripts/leak_tests/test_run_partial_fullsp_debug_skipclimsim_valgrind_r8_longer.py
@@ -15,7 +15,7 @@ acct = 'm4331'
 # case_prefix = 'dagger2_exp1_iter1_alphap5_test'
 # case_prefix = 'corrected_nndebug_prune_clip_seed'
 case_prefix = 'test_run_partial_fullsp_debug_skipclimsim_valgrind_r8_longer'
-# exe_refcase = 'fully_v5_classifier_qnlog'
+exe_refcase = 'test_run_partial_fullsp_debug_skipclimsim_valgrind_r8'
 # Added extra physics_state and cam_out variables.
 
 top_dir  = os.getenv('HOME')
@@ -230,10 +230,13 @@ if config :
    if clean : run_cmd('./case.setup --clean')
    run_cmd('./case.setup --reset')
 #---------------------------------------------------------------------------------------------------
-if build : 
-   if debug_mode: run_cmd('./xmlchange --file env_build.xml --id DEBUG --val TRUE ')
-   if clean : run_cmd('./case.build --clean')
-   run_cmd('./case.build')
+# if build : 
+#    if debug_mode: run_cmd('./xmlchange --file env_build.xml --id DEBUG --val TRUE ')
+#    if clean : run_cmd('./case.build --clean')
+#    run_cmd('./case.build')
+
+run_cmd(f'cp /pscratch/sd/z/zeyuanhu/e3sm_mlt_scratch/{exe_refcase}/build/e3sm.exe ./build/')
+run_cmd('./xmlchange BUILD_COMPLETE=TRUE')
 #---------------------------------------------------------------------------------------------------
 if submit : 
    if 'queue' in locals(): run_cmd(f'./xmlchange JOB_QUEUE={queue}')
